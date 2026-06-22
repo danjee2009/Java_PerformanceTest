4745470e6ca1205f15fc347b8304c4f1b2043fd7
gituser <danjee2009@gmail.com>
2026-06-22 12:07:32 +0900

    h2 의존성 추가

--- src/main/java/com/dgsw/classcheck/controller/StudentController.java
+++ src/main/java/com/dgsw/classcheck/controller/StudentController.java
@@ -1,12 +1,18 @@
 package com.dgsw.classcheck.controller;
 
+import com.dgsw.classcheck.dto.CommonResponse;
 import com.dgsw.classcheck.service.StudentService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
     public StudentController(StudentService studentService) {
     }
+
+    @PostMapping("/add")
+    public CommonResponse addStudent(@RequestBody CommonResponse commonResponse) {}
 
 }
---
+++ src/main/java/com/dgsw/classcheck/dto/CommonResponse.java
@@ -0,0 +1,12 @@
+package com.dgsw.classcheck.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CommonResponse {
+    String message;
+}
