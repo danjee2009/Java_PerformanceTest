b8ca5369872adccc3ef431269afe041d59376062
gituser <danjee2009@gmail.com>
2026-06-22 12:07:32 +0900

    h2 의존성 추가
    # Conflicts:
    #	src/main/java/com/dgsw/classcheck/controller/StudentController.java

--- src/main/java/com/dgsw/classcheck/controller/StudentController.java
+++ src/main/java/com/dgsw/classcheck/controller/StudentController.java
@@ -1,9 +1,9 @@
 package com.dgsw.classcheck.controller;
 
-import com.dgsw.classcheck.dto.ResponseDTO;
+import com.dgsw.classcheck.dto.CommonResponse;
 import com.dgsw.classcheck.service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +11,9 @@ public class StudentController {
     public StudentController(StudentService studentService) {
     }
 
-    @GetMapping("/student")
-    public ResponseDTO
+    @PostMapping("/add")
+    public CommonResponse addStudent(@RequestBody CommonResponse commonResponse) {
+        return new CommonResponse("add new student");
+    }
 
 }
