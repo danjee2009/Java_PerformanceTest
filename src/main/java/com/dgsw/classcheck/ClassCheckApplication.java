cd5a2fca1bfc506aa933c28a62b138647bb20b7f
gituser <danjee2009@gmail.com>
2026-06-22 11:12:02 +0900

    first commit

---
+++ src/main/java/com/dgsw/classcheck/ClassCheckApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.classcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgsw/classcheck/ClassCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.classcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
