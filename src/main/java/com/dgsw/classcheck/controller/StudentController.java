6f34601a40b6c5d62690ad8101931fd9832aaaf1
gituser <danjee2009@gmail.com>
2026-06-23 13:52:53 +0900

    임시 API 생성

--- src/main/java/com/dgsw/classcheck/controller/StudentController.java
+++ src/main/java/com/dgsw/classcheck/controller/StudentController.java
@@ -2,11 +2,14 @@ package com.dgsw.classcheck.controller;
 
 import com.dgsw.classcheck.dto.CommonResponse;
 import com.dgsw.classcheck.dto.StudentRequest;
+import com.dgsw.classcheck.dto.StudentResponse;
 import com.dgsw.classcheck.service.StudentService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -25,6 +28,21 @@ public class StudentController {
     public CommonResponse deleteStudent(@PathVariable Long id) {
         studentService.RemoveStudent();
         return new CommonResponse("delete");
+    }
+
+    @PutMapping("/update")
+    public CommonResponse updateStudent(@PathVariable Long id, @RequestBody StudentRequest studentInfo) {
+
+    }
+
+    @GetMapping("/getAll")
+    public List<StudentResponse> getAllStudent() {
+
+    }
+
+    @GetMapping("/get/{id}")
+    public StudentResponse getStudent(@PathVariable Long id) {
+
     }
 
 }
