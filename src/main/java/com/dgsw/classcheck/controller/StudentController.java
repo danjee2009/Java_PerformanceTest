0af885a9653f76e158979480fc6cc292658f3f5a
gituser <danjee2009@gmail.com>
2026-06-23 13:41:30 +0900

    업데이트/삭제 로직

--- src/main/java/com/dgsw/classcheck/controller/StudentController.java
+++ src/main/java/com/dgsw/classcheck/controller/StudentController.java
@@ -1,19 +1,30 @@
 package com.dgsw.classcheck.controller;
 
 import com.dgsw.classcheck.dto.CommonResponse;
+import com.dgsw.classcheck.dto.StudentRequest;
 import com.dgsw.classcheck.service.StudentService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
+    private final StudentService studentService;
     public StudentController(StudentService studentService) {
+        this.studentService = studentService;
     }
 
     @PostMapping("/add")
-    public CommonResponse addStudent(@RequestBody CommonResponse commonResponse) {
-        return new CommonResponse("add new student");
+    public CommonResponse registerStudent(@RequestBody StudentRequest studentInfo) {
+        studentService.RegisterStudent();
+        return new CommonResponse("add");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResponse deleteStudent(@PathVariable Long id) {
+        studentService.RemoveStudent();
+        return new CommonResponse("delete");
     }
 
 }
--- src/main/java/com/dgsw/classcheck/service/StudentService.java
+++ src/main/java/com/dgsw/classcheck/service/StudentService.java
@@ -3,30 +3,19 @@ package com.dgsw.classcheck.service;
 import com.dgsw.classcheck.dto.StudentInfo;
 import com.dgsw.classcheck.entity.StudentEntity;
 import com.dgsw.classcheck.repository.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 
 @Service
 public class StudentService {
 
     final private StudentRepository studentRepository;
-
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
+    public void RegisterStudent() {}
+    public void RemoveStudent() {}
 
-    public StudentInfo getStudentByClass(int num) {
-        Optional<StudentEntity> optional = studentRepository.findByClassNumber(num);
-
-        if (optional.isPresent()) {
-            StudentEntity studentEntity = optional.get();
-            StudentInfo studentInfo = new StudentInfo();
-
-        }
-
-    }
 
 }
