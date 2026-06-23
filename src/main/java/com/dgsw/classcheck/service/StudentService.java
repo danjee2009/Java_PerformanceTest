08ad997a1de7da9ac96ecbca25718f7fa2522a78
gituser <danjee2009@gmail.com>
2026-06-23 13:55:45 +0900

    유저 조회 서비스 로직 일부 구현

--- src/main/java/com/dgsw/classcheck/service/StudentService.java
+++ src/main/java/com/dgsw/classcheck/service/StudentService.java
@@ -1,9 +1,11 @@
 package com.dgsw.classcheck.service;
 
-import com.dgsw.classcheck.dto.StudentInfo;
+import com.dgsw.classcheck.dto.StudentResponse;
 import com.dgsw.classcheck.entity.StudentEntity;
 import com.dgsw.classcheck.repository.StudentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,6 +18,13 @@ public class StudentService {
 
     public void RegisterStudent() {}
     public void RemoveStudent() {}
+    public List<StudentResponse> getStudentByName(String name) {
+        List<StudentEntity> entities =  studentRepository.findAll();
+    }
+
+    public StudentResponse getStudentById(Long id) {
+        Optional<StudentEntity> entity = studentRepository.findById(id);
+    }
 
 
 }
